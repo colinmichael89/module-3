281f041a4f0af99ce7d997f74d416bc2f2521454
colinmichael89 <colinmichael89@gmail.com>
2022-07-09 19:32:38 -0600

    fixed generate password button to run both functions

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-// Add evenlistener to generateBtn
-generateBtn.addEventListener("click", generatePassword);
 // Create an array with all criteria
 const lowercaseArray = [
   "a",
@@ -94,84 +92,71 @@ function generatePassword() {
     if (characterLength < 8) {
       alert("Must choose a number between 8 and 128");
     } else {
-      // set up conditional to return null if characterLength is not a number (ie user entered non-integer)
-      if (characterLength > 128) {
-        alert("Must choose a number between 8 and 128");
-      } else {
-        if (characterLength > 7 && characterLength < 129) {
-          // research confirm() vs prompt()
+      // Ask if they want upper case letters - confirm
+      var upperCase = confirm(
+        "Do you want uppercase characters in your password?"
+      );
+      console.log(upperCase);
+      // Ask if they want lower case letters - confirm
+      var lowerCase = confirm(
+        "Do you want lowercase characters in your password?"
+      );
+      console.log(lowerCase);
+      // Ask if they want numbers - confirm
+      var numbers = confirm("Do you want numbers in your password?");
+      console.log(numbers);
+      // Ask if they want special characters - confirm
+      var specialCharacters = confirm(
+        "Do you want special characters in your password?"
+      );
+      console.log(specialCharacters);
+      // if conditional - check for length between 8 - 128 characters
+      // make a conditional to check if the user selected any of the above options (confim()  )
 
-          // Ask if they want upper case letters - confirm
-          var upperCase = confirm(
-            "Do you want uppercase characters in your password?"
-          );
-          console.log(upperCase);
-          // Ask if they want lower case letters - confirm
-          var lowerCase = confirm(
-            "Do you want lowercase characters in your password?"
-          );
-          console.log(lowerCase);
-          // Ask if they want numbers - confirm
-          var numbers = confirm("Do you want numbers in your password?");
-          console.log(numbers);
-          // Ask if they want special characters - confirm
-          var specialCharacters = confirm(
-            "Do you want special characters in your password?"
-          );
-          console.log(specialCharacters);
-          // if conditional - check for length between 8 - 128 characters
-          // make a conditional to check if the user selected any of the above options (confim()  )
-
-          if (characterLength > 7 && characterLength < 129) {
-            // if conditional - ask for uppercase is true
-            // push content in uppercase array and add it to new array
-            // if false - skip to next condition
-            if (upperCase === true) {
-              passwordArray = passwordArray.concat(uppercaseArray);
-            }
-            // if conditional - ask for lowercase is true
-            // push content in lowercase array and add it to new array
-            // if false - skip to next condition
-            if (lowerCase === true) {
-              passwordArray = passwordArray.concat(lowercaseArray);
-            }
-            // if conditional - ask for numbers is true
-            // push content in numbers array and add it to new array
-            // if false - skip to next condition
-            if (numbers === true) {
-              passwordArray = passwordArray.concat(numbersArray);
-              // if conditional - ask for special character is true
-              // push content in special character array and add it to new array
-              // if false - end
-              if (specialCharacters === true) {
-                passwordArray = passwordArray.concat(specialcharactersArray);
-              }
-              for (let i = 0; i < characterLength; i++) {
-                password.push(
-                  passwordArray[
-                    Math.floor(Math.random() * passwordArray.length)
-                  ]
-                );
-              }
-            }
+      if (characterLength > 7 && characterLength < 129) {
+        // if conditional - ask for uppercase is true
+        // push content in uppercase array and add it to new array
+        // if false - skip to next condition
+        if (upperCase === true) {
+          passwordArray = passwordArray.concat(uppercaseArray);
+        }
+        // if conditional - ask for lowercase is true
+        // push content in lowercase array and add it to new array
+        // if false - skip to next condition
+        if (lowerCase === true) {
+          passwordArray = passwordArray.concat(lowercaseArray);
+        }
+        // if conditional - ask for numbers is true
+        // push content in numbers array and add it to new array
+        // if false - skip to next condition
+        if (numbers === true) {
+          passwordArray = passwordArray.concat(numbersArray);
+          // if conditional - ask for special character is true
+          // push content in special character array and add it to new array
+          // if false - end
+          if (specialCharacters === true) {
+            passwordArray = passwordArray.concat(specialcharactersArray);
           }
-
-          // Create a for loop that would iterate the character length chosen that will randomly select from our password array
-          console.log(password);
-
-          // Write password to the #password input
-          function writePassword() {
-            var passwordDisplay = generatePassword();
-            var passwordText = document.querySelector("#password");
-
-            passwordText.value = passwordDisplay;
-            password = [];
+          for (let i = 0; i < characterLength; i++) {
+            password.push(
+              passwordArray[Math.floor(Math.random() * passwordArray.length)]
+            );
           }
         }
       }
+
+      // Create a for loop that would iterate the character length chosen that will randomly select from our password array
+      return password;
     }
   }
 }
+// Write password to the #password input
+function writePassword() {
+  var passwordDisplay = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+  passwordText.value = passwordDisplay;
+  password = [];
+}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
